667b6378ea1de1bad5e6ebfd0f0c2c964e13a3e1
Nikola <mts@3i.ua>
2023-01-13 12:43:02 +0200

    added task for check creation

--- SheepFish/order_handler/tasks.py
+++ SheepFish/order_handler/tasks.py
@@ -1,10 +1,28 @@
-import time
-
+from django.template.loader import render_to_string
+from django.core.files import File
+from django.conf import settings
+from .models import Check
 from celery import shared_task
+import json
+import requests
+import base64
 
 
 @shared_task
-def create_task(task_type):
-    time.sleep(int(task_type) * 10)
-    print('run')
+def html_to_pdf(check_id):
+    check = Check.objects.filter(id=check_id).first()
+    context = {'check': check}
+    url = 'http://localhost:8080/'
+    content = render_to_string('OrderDetails.html', context)
+    data = {
+        'contents': base64.b64encode(bytes(content, 'utf-8')).decode(),
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+
+    # Save the response contents to a file
+    check.pdf_file = File(
+        response.content, name=f"{check.order['id']}_{check.type}.pdf")
+    check.status = 'rendered'
+    check.save()
     return True
