9edd7eb122192e6b1c3e282355047e66758125ba
Nikola <mtsebrov@gmail.com>
2023-01-15 16:39:26 +0200

    added filter

--- SheepFish/order_handler/admin.py
+++ SheepFish/order_handler/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Printer, Check
 
-# Register your models here.
+
+def custom_titled_filter(title):
+    class Wrapper(admin.FieldListFilter):
+        def __new__(cls, *args, **kwargs):
+            instance = admin.FieldListFilter.create(*args, **kwargs)
+            instance.title = title
+            return instance
+    return Wrapper
+
+
+class CheckAdmin(admin.ModelAdmin):
+    list_filter = [
+        ("printer_id", custom_titled_filter('принтером')),
+        ("type", custom_titled_filter('типом')),
+        ("status", custom_titled_filter('статусом'))
+    ]
+    search_fields = (
+        "type",
+        "status",
+    )
+
+
+admin.site.register(Check, CheckAdmin)
 admin.site.register(Printer)
-admin.site.register(Check)
+# admin.site.register(Check)
