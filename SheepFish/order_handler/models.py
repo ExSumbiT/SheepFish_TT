fbdfac211f96fea8f5db6fb4cd9d48219b35afc5
Nikola <mtsebrov@gmail.com>
2023-01-15 16:40:04 +0200

    added serializer for Check and __str__ methods

--- SheepFish/order_handler/models.py
+++ SheepFish/order_handler/models.py
@@ -10,6 +10,9 @@ class Printer(models.Model):
     check_type = models.CharField(max_length=8, choices=check_types)
     point_id = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f'{self.point_id}. {self.name} | {self.check_type}'
+
 
 class Check(models.Model):
     printer_id = models.ForeignKey('Printer', on_delete=models.CASCADE)
@@ -18,3 +21,14 @@ class Check(models.Model):
     status = models.CharField(max_length=8, choices=(
         ('new', 'New'), ('rendered', 'Rendered'), ('printed', 'Printed')))
     pdf_file = models.FileField(upload_to='pdf/')
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "api_key": self.printer_id.api_key,
+            "type": self.type,
+            "status": self.status,
+            "pdf_file": self.pdf_file.url}
+
+    def __str__(self) -> str:
+        return f'{self.id}. Чек №{self.order["id"]} | {self.type} | {self.status}'
